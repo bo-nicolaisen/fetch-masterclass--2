b4a25c38703b704a1cca6a0dc86d1746fd381e12
bo-nicolaisen <bni@techcollege.dk>
2023-09-06 13:16:19 +0200

    done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,47 @@
-function
+const apiUrl = 'https://dog.ceo/api/breeds/image/random/1';
+
+fetch(apiUrl)
+
+
+
+    .then(response => {
+
+        console.table(response);
+
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+
+
+        return response.json(); // Parse the response body as JSON
+    })
+
+
+    .then(data => {
+
+        // Process the data here
+        console.table(data.message);
+
+
+        // You can access the dog images from data.message
+        const dogImages = data.message;
+
+
+
+        // Display the dog images (assuming you have an HTML element with id "dog-images")
+        const dogImagesElement = document.getElementById('dog-images');
+
+        if (dogImagesElement) {
+
+            dogImages.map(imageUrl => {
+                const img = document.createElement('img');
+                img.src = imageUrl;
+                dogImagesElement.appendChild(img);
+            });
+        }
+    })
+
+
+    .catch(error => {
+        console.error('Error:', error);
+    }); 
